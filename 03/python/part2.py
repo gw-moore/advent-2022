e7ea539e31f543c8b6067010c46f21d7f5e724aa
gw-moore <gwmoore.career@gmail.com>
2022-12-04 13:17:52 -0500

    Add day 3 part 2 in python

--- 03/python/part2.py
+++ 03/python/part2.py
@@ -3,6 +3,7 @@ import string
 
 path = Path(__file__).parents[1] / "data.txt"
 rucksacks: list[str] = path.read_text().split("\n")
+groups: list[list[str]] = [rucksacks[i : i + 3] for i in range(0, len(rucksacks), 3)]
 
 priority_map = dict(
     zip(string.ascii_lowercase, [ord(c) % 32 for c in string.ascii_lowercase])
@@ -13,18 +14,7 @@ upper_priority_map = dict(
 priority_map.update(upper_priority_map)
 
 total_cost = 0
-for ruck in rucksacks:
-    # ensure that each compartment is of equal length
-    assert len(ruck) % 2 == 0
-
-    # determine the size of the compartment for the given ruck
-    comp_len = int(len(ruck) / 2)
-    comp1 = ruck[0:comp_len]
-    comp2 = ruck[comp_len:]
-
-    # add the cost to the running total
-    shared_elements = list(set(comp1).intersection(comp2))
-    for se in shared_elements:
-        total_cost += priority_map[se]
-
+for grp in groups:
+    unique_value = list(set(set(grp[0]) & set(grp[1]) & set(grp[2])))
+    total_cost += priority_map[unique_value[0]]
 print(f"Total cost: {total_cost}")
