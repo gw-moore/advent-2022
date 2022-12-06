b05ade595c9818c3b5adb9200d3753c9dd2f40c5
gw-moore <gwmoore.career@gmail.com>
2022-12-06 17:20:00 -0500

    Day 5 & 6 in python

---
+++ 05/python/part1.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from pathlib import Path
+from rich.pretty import pprint
+
+
+@dataclass
+class Move:
+    n_crates: int
+    source: str
+    destination: str
+
+    @classmethod
+    def from_str(cls, string: str):
+        l = [int(s) for s in string if s.isdigit()]
+        return cls(n_crates=l[0], source=f"stack{l[1]}", destination=f"stack{l[2]}")
+
+
+path = Path(__file__).parents[1] / "instructions.txt"
+raw_instructions = Path(path).read_text().split("\n")
+raw_instructions = [i.split(" ") for i in raw_instructions]
+instructions = [Move.from_str(s) for s in raw_instructions]
+
+#     [P]                 [Q]     [T]
+# [F] [N]             [P] [L]     [M]
+# [H] [T] [H]         [M] [H]     [Z]
+# [M] [C] [P]     [Q] [R] [C]     [J]
+# [T] [J] [M] [F] [L] [G] [R]     [Q]
+# [V] [G] [D] [V] [G] [D] [N] [W] [L]
+# [L] [Q] [S] [B] [H] [B] [M] [L] [D]
+# [D] [H] [R] [L] [N] [W] [G] [C] [R]
+#  1   2   3   4   5   6   7   8   9
+
+stacks = {
+    "stack1": ["D", "L", "V", "T", "M", "H", "F"],
+    "stack2": ["H", "Q", "G", "J", "C", "T", "N", "P"],
+    "stack3": ["R", "S", "D", "M", "P", "H"],
+    "stack4": ["L", "B", "V", "F"],
+    "stack5": ["N", "H", "G", "L", "Q"],
+    "stack6": ["W", "B", "D", "G", "R", "M", "P"],
+    "stack7": ["G", "M", "N", "R", "C", "H", "L", "Q"],
+    "stack8": ["C", "L", "W"],
+    "stack9": ["R", "D", "L", "Q", "J", "Z", "M", "T"],
+}
+
+
+def apply_instructions(stacks: dict, instructions: list[Move]) -> dict:
+    for move in instructions:
+        for _ in range(int(move.n_crates)):
+            letter = stacks[move.source].pop(-1)
+            stacks[move.destination].append(letter)
+    return stacks
+
+
+results = apply_instructions(stacks, instructions)
+print()
+print("Final result...")
+pprint(results)
+
+print("final string...")
+fs = ""
+for k, v in results.items():
+    fs = fs + v[-1]
+print(fs)
---
+++ 06/python/part2.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+path = Path(__file__).parents[1] / "data.txt"
+buff = Path(path).read_text()
+
+
+def id_start_of_message_marker(buff: str):
+    """Find the first 14 non-repeating character in a string and return the index at the start of the id"""
+
+    buff_list = list(buff)
+    counter = 0
+    uid = ""
+    # build the uid
+    for _ in buff:
+        while len(uid) < 15:
+            counter += 1
+            uid = uid + buff_list.pop(0)
+        # once the uid is 4 character long, check if it is unique
+        x = list(set(uid))
+        y = list(uid)
+        x.sort()
+        y.sort()
+        if x == y:
+            return counter - 1, uid
+        # if not unique, remove the first char from the string and start over
+        uid = uid[1:]
+
+
+index, uid = id_start_of_message_marker(buff)
+print(f"The index for the marker is: {index}")
+print(f"The id is: {uid}")
