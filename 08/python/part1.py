b9f96324d8eeecc773ad60ea34f25678cdff0bd9
gw-moore <gwmoore.career@gmail.com>
2022-12-10 10:05:06 -0500

    Day 8 part 1 in python

---
+++ 08/python/part1.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from pathlib import Path
+from rich import print
+from rich.pretty import pprint
+from dataclasses import dataclass, field
+import numpy as np
+
+path = Path(__file__).parents[1] / "data.txt"
+lines = []
+for line in Path(path).read_text().split("\n"):
+    lines.append([int(s) for s in line])
+
+# lines = [
+#     [int(s) for s in "30373"],
+#     [int(s) for s in "25512"],
+#     [int(s) for s in "65332"],
+#     [int(s) for s in "33549"],
+#     [int(s) for s in "35390"],
+# ]
+
+matrix = np.array(lines)
+
+# question: how many trees are visible from outside the grid?
+
+# first, count the number of trees that make up the perimeter
+nrows, ncols = matrix.shape
+num_viz_trees = (nrows * 2) + (ncols * 2) - 4
+
+num_viz_trees = (nrows * 2) + (ncols * 2) - 4
+print(f"starting num_viz_tree: {num_viz_trees}")
+
+for i in list(range(matrix.shape[0])):
+    if i == 0 or i == nrows - 1:
+        continue
+    for j in list(range(matrix.shape[1])):
+        if j == 0 or j == ncols - 1:
+            continue
+
+        tree = matrix[i, j]
+        ABOVE = list(range(0, i))
+        BELOW = list(range(i + 1, nrows))
+        LEFT = list(range(0, j))
+        RIGHT = list(range(j + 1, ncols))
+
+        print()
+        print(f"tree pos: {i, j}")
+        print(f"tree size: {tree}")
+
+        hidden_above = any([tree <= matrix[a, j] for a in ABOVE])
+        hidden_below = any([tree <= matrix[b, j] for b in BELOW])
+        hidden_left = any([tree <= matrix[i, l] for l in LEFT])
+        hidden_right = any(tree <= matrix[i, r] for r in RIGHT)
+        print(f"Hidden above: {hidden_above}")
+        print(f"Hidden below: {hidden_below}")
+        print(f"Hidden left: {hidden_left}")
+        print(f"Hidden right: {hidden_right}")
+
+        hidden = all([hidden_above, hidden_below, hidden_left, hidden_right])
+        print(f"Hidden: {hidden}")
+        if hidden is False:
+            num_viz_trees += 1
+        print(f"num viz trees: {num_viz_trees}")
+
+print()
+print(num_viz_trees)
